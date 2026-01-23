6fa115d418dbae42a1497c523c5b010f304f8b83
blooveMinecraft <snayck.parant@gmail.com>
2026-01-23 16:13:47 +0100

    Update music.js

--- script/music.js
+++ script/music.js
@@ -4,14 +4,19 @@ const icon = document.getElementById("music-icon");
 
 let playing = false;
 
+// Débloque l'audio dès le premier clic sur la page
+document.addEventListener("click", () => {
+    music.muted = false;
+}, { once: true });
+
 btn.addEventListener("click", () => {
-  if (!playing) {
-    music.play();
-    icon.src = "images/Musique activé.png";
-    playing = true;
-  } else {
-    music.pause();
-    icon.src = "images/Musique désactivé.png";
-    playing = false;
-  }
+    if (!playing) {
+        music.play().catch(err => console.log(err));
+        icon.src = "images/Musique activé.png";
+        playing = true;
+    } else {
+        music.pause();
+        icon.src = "images/Musique désactivé.png";
+        playing = false;
+    }
 });
